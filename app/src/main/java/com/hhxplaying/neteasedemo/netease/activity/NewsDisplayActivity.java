f0d2757480bb4f3da27c68010c2b162ed9c00a17
NetEase <netease@NetEasedeiMac-4.local>
2016-02-18 21:18:57 +0800

    完成新闻详情界面的主要技术实现的DEMO

--- app/src/main/java/com/hhxplaying/neteasedemo/netease/activity/NewsDisplayActivity.java
+++ app/src/main/java/com/hhxplaying/neteasedemo/netease/activity/NewsDisplayActivity.java
@@ -1,25 +1,35 @@
 package com.hhxplaying.neteasedemo.netease.activity;
 
 import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LevelListDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.hhxplaying.neteasedemo.netease.R;
+import com.hhxplaying.neteasedemo.netease.util.URLImageParser;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+import org.w3c.dom.Text;
 
 
 /**
  * Created by HHX on 15/9/10.
  */
-public class NewsDisplayActivity extends AppCompatActivity {
+public class NewsDisplayActivity extends AppCompatActivity implements Html.ImageGetter {
     private SystemBarTintManager tintManager;
 
+    private TextView content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +38,47 @@ public class NewsDisplayActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(" ");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setTitle(" ");
+            bar.setDisplayHomeAsUpEnabled(true);
+            bar.setHomeButtonEnabled(true);
+        }
+
+
+        String html = "Hello \n" +
+                "<img src='http://ww1.sinaimg.cn/mw600/4dc7b570jw1drn1o8mrp0j.jpg' />" +
+                " This is a test \n" +
+                "<img src='http://att.bbs.duowan.com/forum/201311/01/0950172al0qkazlh20hh9n.png'/>";
+
+
+        content = (TextView)findViewById(R.id.tv_content);
+        URLImageParser p = new URLImageParser(content, this);
+        Spanned htmlSpan = Html.fromHtml(html, p, null);
+        content.setText(htmlSpan);
+
     }
+
+    @Override
+    public Drawable getDrawable(String arg0) {
+        // TODO Auto-generated method stub
+        int id = 0;
+
+        if(arg0.equals("addbutton.png")){
+            id = R.drawable.load_fail_black;
+        }
+
+        if(arg0.equals("tu1.png")){
+            id = R.drawable.load_fail_black;
+        }
+        LevelListDrawable d = new LevelListDrawable();
+        Drawable empty = getResources().getDrawable(id);
+        d.addLevel(0, 0, empty);
+        d.setBounds(0, 0, empty.getIntrinsicWidth(), empty.getIntrinsicHeight());
+
+        return d;
+    }
+
 
     @TargetApi(19)
     private void initWindow() {
---
+++ app/src/main/java/com/hhxplaying/neteasedemo/netease/util/URLImageParser.java
@@ -0,0 +1,143 @@
+package com.hhxplaying.neteasedemo.netease.util;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.hhxplaying.neteasedemo.netease.MyApplication;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by netease on 16/2/18.
+ */
+public class URLImageParser implements Html.ImageGetter {
+    Context c;
+    TextView container;
+
+    /***
+     * Construct the URLImageParser which will execute AsyncTask and refresh the container
+     * @param t
+     * @param c
+     */
+    public URLImageParser(TextView t, Context c) {
+        this.c = c;
+        this.container = t;
+    }
+
+    public Drawable getDrawable(String source) {
+        URLDrawable urlDrawable = new URLDrawable();
+
+        // get the actual source
+        ImageGetterAsyncTask asyncTask =
+                new ImageGetterAsyncTask( urlDrawable);
+
+        asyncTask.execute(source);
+
+        // return reference to URLDrawable where I will change with actual image from
+        // the src tag
+        return urlDrawable;
+    }
+
+    public class ImageGetterAsyncTask extends AsyncTask<String, Void, Drawable> {
+        URLDrawable urlDrawable;
+
+        public ImageGetterAsyncTask(URLDrawable d) {
+            this.urlDrawable = d;
+        }
+
+        @Override
+        protected Drawable doInBackground(String... params) {
+            String source = params[0];
+            return fetchDrawable(source);
+        }
+
+        @Override
+
+        protected void onPostExecute(Drawable result) {
+            // set the correct bound according to the result from HTTP call
+            Log.i("RVA",""+result.getIntrinsicHeight());
+            Log.i("RVA", "" + result.getIntrinsicWidth());
+            //设置图片显示占据的高度
+            urlDrawable.setBounds(0, 0, 0 + MyApplication.width, 0+result.getIntrinsicHeight() * MyApplication.width / result.getIntrinsicWidth());
+
+            // change the reference of the current drawable to the result
+            // from the HTTP call
+            urlDrawable.drawable = result;
+
+            // redraw the image by invalidating the container
+            URLImageParser.this.container.invalidate();
+
+            // For ICS
+            URLImageParser.this.container.setHeight((URLImageParser.this.container.getHeight()
+                    + result.getIntrinsicHeight()));
+
+            // Pre ICS
+            URLImageParser.this.container.setEllipsize(null);
+
+        }
+
+        /***
+         * Get the Drawable from URL
+         * @param urlString
+         * @return
+         */
+        public Drawable fetchDrawable(String urlString) {
+            InputStream is = null;
+            try {
+                is = fetch(urlString);
+                Drawable drawable = Drawable.createFromStream(is, "src");
+
+                //设置图片的实际高低
+                drawable.setBounds(0, 0, 0 + MyApplication.width, 0
+                        + drawable.getIntrinsicHeight()* MyApplication.width / drawable.getIntrinsicWidth());
+                return drawable;
+            } catch (Exception e) {
+                if (is != null)
+                    try {
+                        is.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                return null;
+            }
+        }
+
+        private InputStream fetch(String urlString) throws IOException {
+            URL url = new URL(urlString);
+            Log.i("RVA", urlString);
+            URLConnection urlConnection = url.openConnection();
+            return new BufferedInputStream(urlConnection.getInputStream());
+        }
+    }
+
+
+    class URLDrawable extends BitmapDrawable {
+        // the drawable that you need to set, you could set the initial drawing
+        // with the loading image if you need to
+        protected Drawable drawable;
+
+        @Override
+        public void draw(Canvas canvas) {
+            // override the draw to facilitate refresh function later
+            if(drawable != null) {
+                drawable.draw(canvas);
+            }
+        }
+    }
+}
